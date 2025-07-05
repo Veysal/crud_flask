b4d72e58104d4a9edb0996a4dee2cf3ac5157192
Veysal <tveysal93@mail.ru>
2025-07-05 11:18:49 +0300

    Создали маршрут для add_user.html. Добавили возможность добавлять данные в таблицу users

--- app.py
+++ app.py
@@ -38,3 +38,26 @@ def main():
 
     conn.close()
     return render_template("index.html")
+
+@app.route("/add", methods = ["GET", "POST"])
+def add_user():
+    if request.method == "POST":
+        first_name = request.form.get("first_name")
+        last_name = request.form.get("last_name")
+        gender = request.form.get("gender")
+        age = int(request.form.get("age"))
+        phone = request.form.get("phone")
+        email = request.form.get("email")
+        address = request.form.get("addres")
+        salary = float(request.form.get("salary"))
+
+        conn = get_db_conn()
+        conn.execute("""
+            INSERT INTO users(first_name, last_name, gender, age, phone, email, address, salary)
+            VALUES(?,?,?,?,?,?,?,?)
+        """, (first_name, last_name, gender, age, phone, email, address, salary))
+        conn.commit()
+        conn.close()
+        return redirect(url_for('main'))
+    return render_template("add_user.html")
+
